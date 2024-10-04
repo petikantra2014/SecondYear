0daf2d3604d1ce53e4eb3fb80f3dc193587d329f
petikantra2014 <antonkutergin123@gmail.com>
2024-10-04 16:25:15 +0300

    123

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -10,50 +10,47 @@ private:
     T* _array;
     size_t _size;
     size_t _count;
-    int min = 1000000000;
-    int max = -1000000000;
 public:
-    ArrayHandler(){
-        _size = 5;
-        _count = 0;
-        _array = new T[_size];
-    };
-    ArrayHandler(size_t size){
+    ArrayHandler(size_t size = 10){
         _size = size;
         _count = 0;
         _array = new T[_size];
     };
     void AppendElem(T elem){
         if (_count == _size){
-            T* array = new T[_size];
+            T* array = new T[_size * _size];
             for(int i = 0; i < _size; i++)
             array[i] = _array[i];
             delete [] _array;
-            _size++;
+            _size = _size * _size;
             _array = array;
         }
         
         _array[_count++] = elem;
 
-        if (max < elem){
-            max = elem;
-        }
-        if (min > elem){
-            min = elem;
-        }
     };
-    bool FindElem(const T& elem) const {
-        for(int i = 0; i < _count; i++){
+    bool IsContains(const T& elem) const {
+        for(size_t i = 0; i < _count; i++){
             if(elem == _array[i]) return true;
         }
         return false;
     };
     T GetMax(){
-    return max;
+        T max = _array[0];
+        for(size_t i = 1; i < _count; i++)
+        {
+            if (_array[i] > max) max = _array[i];
+        }
+        return max;
     };
 
     T GetMin(){
-    return min;
+        T min = _array[0];
+        for(size_t i = 1; i < _count; i++)
+        {
+            if (_array[i] < min) min = _array[i];
+        }
+        return min;
     };
     ~ArrayHandler(){
         delete [] _array;
