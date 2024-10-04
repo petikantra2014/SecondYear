5cc24d05768fc414a3eac33d54cf0abf05bc3420
petikantra2014 <antonkutergin123@gmail.com>
2024-10-04 15:30:01 +0300

    petikantra2014

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,32 +1,61 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+
 
 template<typename T>
-class ArrayHandler {
+class ArrayHandler
+{
 private:
-    size_t _size;
     T* _array;
+    size_t _size;
+    size_t _count;
+    int min = 1000000000;
+    int max = -1000000000;
 public:
-    ArrayHandler(size_t size = 10) {
+    ArrayHandler(){
+        _size = 5;
+        _count = 0;
+        _array = new T[_size];
+    };
+    ArrayHandler(size_t size){
+        _size = size;
+        _count = 0;
+        _array = new T[_size];
+    };
+    void AppendElem(T elem){
+        if (_count == _size){
+            T* array = new T[_size];
+            for(int i = 0; i < _size; i++)
+            array[i] = _array[i];
+            delete [] _array;
+            _size++;
+            _array = array;
+        }
+        
+        _array[_count++] = elem;
 
-    }
-
-    void AppendElem(T elem) {
-
-    }
-
-    bool IsContains(T elem) {
+        if (max < elem){
+            max = elem;
+        }
+        if (min > elem){
+            min = elem;
+        }
+    };
+    bool FindElem(const T& elem) const {
+        for(int i = 0; i < _count; i++){
+            if(elem == _array[i]) return true;
+        }
         return false;
-    }
+    };
+    T GetMax(){
+    return max;
+    };
 
-    T GetMax() {
-        return 0;
-    }
-
-    T GetMin() {
-        return 0;
-    }
-
-    ~ArrayHandler() {
-
-    }
-
+    T GetMin(){
+    return min;
+    };
+    ~ArrayHandler(){
+        delete [] _array;
+    };
 };
